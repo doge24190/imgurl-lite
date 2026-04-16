ecb9a96bcfa80576af6f5ed4738a3609af63495f
doge24190 <69849296+doge24190@users.noreply.github.com>
2026-04-16 14:40:55 +0800

    删减后台多于显示

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -33,40 +33,10 @@
 		</div>
 		<div class="layui-col-lg3">
 			<div class = "analyze">
-				<h3><i class="fa fa-comments"></i> 社区支持</h3>
-				<p><a href="https://dwz.ovh/imgurl2" target = "_blank" title = "ImgURL社区支持">https://ttt.sh/</a></p>
-			</div>
-		</div>
-		<div class="layui-col-lg3">
-			<div class = "analyze">
 				<h3><i class="fa fa-github"></i> 源码下载</h3>
-				<p><a href="https://github.com/helloxz/imgurl" target = "_blank">https://github.com/</a></p>
+				<p><a href="https://github.com/doge24190/imgurl-lite" target = "_blank">https://github.com/</a></p>
 			</div>
 		</div>
-		<div class="layui-col-lg3">
-			<div class = "analyze">
-				<h3><i class="fa fa-paypal"></i> 打赏支持</h3>
-				<p><a href="https://dwz.ovh/imgurl" target = "_blank">https://dwz.ovh/imgurl</a></p>
-			</div>
-		</div>
-		<div class="layui-col-lg3">
-			<div class = "analyze">
-				<h3><i class="fa fa-qq"></i> 技术支持（付费）</h3>
-				<p>QQ:<a href = "http://wpa.qq.com/msgrd?v=3&uin=337003006&site=qq&menu=yes" rel = "nofollow" target = "_blank" title = "联系作者，获得技术支持。">337003006</a></p>
-			</div>
-		</div>
-		<div class="layui-col-lg3">
-			<div class = "analyze">
-				<h3><i class="fa fa-qq"></i> QQ交流群</h3>
-				<p><a href = "//shang.qq.com/wpa/qunwpa?idkey=1994c352ea864ed09a00fd3cfb0f80c946850a70e98551736be69f4253f09136" rel = "nofollow" target = "_blank" title = "加入ImgURL交流群，和大家一起讨论。">341195621</a></p>
-			</div>
-		</div>
-        <div class="layui-col-lg3">
-            <div class = "analyze">
-                <h3><i class="fa fa-paypal"></i> 购买专业版</h3>
-                <p><a href = "https://dwz.ovh/9" rel = "nofollow" target = "_blank" title = "ImgURL Pro专业版，功能更加强大。">https://dwz.ovh/9</a></p>
-            </div>
-        </div>
 	</div>
 </div>
 
